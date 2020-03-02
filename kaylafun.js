cc3d7405ebbe1c76ca66d3ddf266320a7e49a4c0
Kayla Mauriello <kmauriel@gmail.com>
2020-03-01 21:28:48 -0700

    Added slider html, css, js.

--- kaylafun.js
+++ kaylafun.js
@@ -4,11 +4,11 @@
 
 $(window).scroll(function() {
 
-    if ($(window).scrollTop() > 300) {
-        $('.main_nav').addClass('sticky');
-    } else {
-        $('.main_nav').removeClass('sticky');
-    }
+  if ($(window).scrollTop() > 300) {
+    $('.main_nav').addClass('sticky');
+  } else {
+    $('.main_nav').removeClass('sticky');
+  }
 });
 
 /*----------------------------------------------------*/
@@ -17,20 +17,77 @@ $(window).scroll(function() {
 
 jQuery(document).ready(function($) {
 
-   $('.smoothscroll').on('click',function (e) {
-	    e.preventDefault();
+  $('.smoothscroll').on('click', function(e) {
+    e.preventDefault();
 
-	    var target = this.hash,
-	    $target = $(target);
+    var target = this.hash,
+      $target = $(target);
 
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 800, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
-  
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+    }, 800, 'swing', function() {
+      window.location.hash = target;
+    });
+  });
+
 });
 
 
-TweenMax.staggerFrom(".heading", 0.8, {opacity: 0, y: 20, delay: 0.2}, 0.4);
+TweenMax.staggerFrom(".heading", 0.8, {
+  opacity: 0,
+  y: 20,
+  delay: 0.2
+}, 0.4);
+
+/*testimonial slider functions*/
+var current = 1;
+var max = $("li").length + 1;
+
+$(".slidercontent").hide();
+$(".slidercontent:nth-child(1)").fadeIn("slow");
+
+//function to change to next quote
+function changeUp() {
+  $(".slidercontent").hide();
+  current += 1;
+  if (current === max) {
+    current = 1;
+  }
+  $(".slidercontent:nth-child(" + current + ")").fadeIn("slow");
+}
+
+function changeDown() {
+  $(".slidercontent").hide();
+  current -= 1;
+
+  if (current === 0) {
+    current = max - 1;
+  }
+
+  $(".slidercontent:nth-child(" + current + ")").fadeIn("slow");
+}
+
+startChange();
+
+$(".sliderspot2").click(function() {
+  stopChange();
+  changeUp();
+  startChange();
+});
+
+$(".sliderspot").click(function() {
+  stopChange();
+  changeDown();
+  startChange();
+});
+
+//FUNCTIONS TO CONTROL TIMING CHANGES
+function startChange() {
+  changeIt = setInterval(function() {
+    changeUp();
+  }, 10000);
+};
+
+function stopChange() {
+  clearInterval(changeIt);
+}
