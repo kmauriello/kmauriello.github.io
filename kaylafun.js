f95afd730319a0cc761e24d0e52dcb048d2e13b8
Kayla Mauriello <kmauriel@gmail.com>
2020-03-04 21:28:01 -0700

    FIXED STUFF.

--- kaylafun.js
+++ kaylafun.js
@@ -1,47 +1,55 @@
 /*----------------------------------------------------*/
 /* Navigation
 ------------------------------------------------------ */
-
+/*
 $(window).scroll(function() {
 
-      if ($(window).scrollTop() > 300) {
-        $('.main_nav').addClass('sticky');
-      } else {
-        $('.main_nav').removeClass('sticky');
-      }
+  if ($(window).scrollTop() > 300) {
+    $('.main_nav').addClass('sticky');
+  } else {
+    $('.main_nav').removeClass('sticky');
+  }
+});
 
-      /*----------------------------------------------------*/
-      /* Smooth Scrolling
-      ------------------------------------------------------ */
+/*----------------------------------------------------*/
+/* Smooth Scrolling
+------------------------------------------------------ */
+/*
+jQuery(document).ready(function($) {
 
-      jQuery(document).ready(function($) {
+  $('.smoothscroll').on('click', function(e) {
+    e.preventDefault();
 
-            $('.smoothscroll').on('click', function(e) {
-                  e.preventDefault();
+    var target = this.hash,
+      $target = $(target);
 
-                  var target = this.hash,
-                    $target = $(target);
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+    }, 800, 'swing', function() {
+      window.location.hash = target;
+    });
+  });
 
-                  $('html, body').stop().animate({
-                      'scrollTop': $target.offset().top
-                    }, 800, 'swing', function() {
-                      window.location.hash = target;
-                    }
+});
 
-                    TweenMax.staggerFrom(".heading", 0.8, {
-                      opacity: 0,
-                      y: 20,
-                      delay: 0.2
-                    }, 0.4);
-                  }
-                  /*testimonial slider functions*/
-                  $(document).ready(function() {
 
-                        $(function() {
+TweenMax.staggerFrom(".heading", 0.8, {
+  opacity: 0,
+  y: 20,
+  delay: 0.2
+}, 0.4);
+*/
+/*testimonial slider functions*/
+$(document).ready(function() {
 
-                              $('.slides-container .slides:gt(0)').hide();
-                              setInterval(function() {
-                                $('.slides-container :first-child').fadeOut(500).next('.slides').fadeIn(500)
-                                  .end().appendTo('.slides-container');
-                              }, 3000);
-                            }
+  $(function() {
+
+    $('.slides-container .slides:gt(0)').hide();
+    setInterval(function() {
+      $('.slides-container :first-child').fadeOut(500).next('.slides').fadeIn(500)
+        .end().appendTo('.slides-container');
+    }, 3000);
+
+  });
+
+});
