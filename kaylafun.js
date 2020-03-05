0ce1129c025ab26647f48243442ec12529e8cdcc
Kayla Mauriello <kmauriel@gmail.com>
2020-03-04 21:15:07 -0700

    stuff

--- kaylafun.js
+++ kaylafun.js
@@ -1,55 +1,47 @@
 /*----------------------------------------------------*/
 /* Navigation
 ------------------------------------------------------ */
-/*
+
 $(window).scroll(function() {
 
-  if ($(window).scrollTop() > 300) {
-    $('.main_nav').addClass('sticky');
-  } else {
-    $('.main_nav').removeClass('sticky');
-  }
-});
+      if ($(window).scrollTop() > 300) {
+        $('.main_nav').addClass('sticky');
+      } else {
+        $('.main_nav').removeClass('sticky');
+      }
 
-/*----------------------------------------------------*/
-/* Smooth Scrolling
------------------------------------------------------- */
-/*
-jQuery(document).ready(function($) {
+      /*----------------------------------------------------*/
+      /* Smooth Scrolling
+      ------------------------------------------------------ */
 
-  $('.smoothscroll').on('click', function(e) {
-    e.preventDefault();
+      jQuery(document).ready(function($) {
 
-    var target = this.hash,
-      $target = $(target);
+            $('.smoothscroll').on('click', function(e) {
+                  e.preventDefault();
 
-    $('html, body').stop().animate({
-      'scrollTop': $target.offset().top
-    }, 800, 'swing', function() {
-      window.location.hash = target;
-    });
-  });
+                  var target = this.hash,
+                    $target = $(target);
 
-});
+                  $('html, body').stop().animate({
+                      'scrollTop': $target.offset().top
+                    }, 800, 'swing', function() {
+                      window.location.hash = target;
+                    }
 
+                    TweenMax.staggerFrom(".heading", 0.8, {
+                      opacity: 0,
+                      y: 20,
+                      delay: 0.2
+                    }, 0.4);
+                  }
+                  /*testimonial slider functions*/
+                  $(document).ready(function() {
 
-TweenMax.staggerFrom(".heading", 0.8, {
-  opacity: 0,
-  y: 20,
-  delay: 0.2
-}, 0.4);
-*/
-/*testimonial slider functions*/
-$(document).ready(function() {
+                        $(function() {
 
-  $(function() {
-
-    $('.slides-container .slides:gt(0)').hide();
-    setInterval(function() {
-      $('.slides-container :first-child').fadeOut(500).next('.slides').fadeIn(500)
-        .end().appendTo('.slides-container');
-    }, 3000);
-
-  });
-
-});
+                              $('.slides-container .slides:gt(0)').hide();
+                              setInterval(function() {
+                                $('.slides-container :first-child').fadeOut(500).next('.slides').fadeIn(500)
+                                  .end().appendTo('.slides-container');
+                              }, 3000);
+                            }
